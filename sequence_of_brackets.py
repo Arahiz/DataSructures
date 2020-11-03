e5995755b3523fc8f17627688e5416b0e7cc1a2e
Maxim.Bubnov <maksim.ungern@gmail.com>
2020-11-03 16:02:14 +0300

    added error index output

--- sequence_of_brackets.py
+++ sequence_of_brackets.py
@@ -5,18 +5,25 @@ from  ds_stack import *
 
 def Isbalanced(str):
     stack = Stack()
+    stack_index_open_brackets = Stack()
+    count = 0
     for char in str:
+        count += 1
         if char != '(' and char != ')' and char != '[' and char != ']' and char != '{' and char != '}':
             continue
         if char == '(' or char == '[' or char == '{':
             stack.push(char)
+            stack_index_open_brackets.push(count)
             continue
         if stack.empty():
-            return False
+            return count
         top = stack.pop()
         if top == '(' and char != ')' or top == '[' and char != ']' or top == '{' and char != '}':
-            return False
-    return stack.empty()
+            return count
+        stack_index_open_brackets.pop()
+    if not  stack.empty():
+        return stack_index_open_brackets.pop()
+    return "Success"
 
 if __name__ == '__main__':
     print(Isbalanced(input()))
